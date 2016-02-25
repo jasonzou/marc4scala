a287759c455e1957acb294bbcc3335710927e76d
jasonzou <jason.zou@gmail.com>
2016-02-24 21:59:53 -0500

    LeaderTest.scala

--- src/test/scala/org/marc4scala/LeaderTest.scala
+++ src/test/scala/org/marc4scala/LeaderTest.scala
@@ -1,19 +1,17 @@
 package org.marc4scala
 
-import org.marc4scala.Leader;
+import org.marc4scala.Leader
+import org.scalatest.FlatSpec
 
-Class LeaderTest extends FlatSpec{
- Def testUnmarshal() {
-   Leader leader = factory.newLeader();
-   leader.unmarshal("00714cam a2200205 a 4500")
-   assertEquals("00714cam a2200205 a 4500"
- Leader leader = factory.newLeader();
-   leader.unmarshal("00714cam a2100205 a 4500");
-    assertEquals(1, leader.)
- Def testMarshal() {
-                                                                                                                  
-   Leader leader = factory.newLeader("00714cam a2200205 a 4500");
-                                                                                                                          assertEquals("00714cam a2200205 a 4500", leader.marshal());
-                                                                                                                              }
+class LeaderTest extends FlatSpec{
+  val leader = new Leader
+  
+  Behavior of "Leader"
+  
+  "Leader" must "unmarshal correctly" in {
+     leader.unmarshal("00714cam a2200205 a 4500")
+     assertEquals("00714cam a2200205 a 4500", leader.toString)
+  }
 
+  it must "marshal correctly" is (pending)
 }
