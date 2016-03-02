f59d4f69084fe5ad5d6dd0ebaab316440464228d
Jason Zou <Jason.zou@gmail.com>
2016-03-01 20:14:43 -0500

    Update Tag.scala

--- src/main/scala/org/marc4scala/Tag.scala
+++ src/main/scala/org/marc4scala/Tag.scala
@@ -24,5 +24,68 @@ package org.marc4scala
   * Created by jason on 3/1/16.
   */
 class Tag {
+        /// <summary>
+        /// This class is for MARC tags.
+        /// </summary>
+        
+        private char[] buff = new char[3] { '0', '0', '0' };
+        public string _warn;
 
+        public MARCTag(string str)
+        {
+            bool test;
+
+            test = Regex.IsMatch(str, "^[0-9A-Z]{3}$");
+            test |= Regex.IsMatch(str, "^[0-9a-z]{3}$");
+            if (test)
+            {
+                str.CopyTo(0, buff, 0, 3);
+            }
+            else
+            {
+                _warn = "Invalid Tagno";
+            }
+        }
+
+        public string tagStr
+        {
+            get
+            {
+                string temp;
+                temp = new String(buff);
+                return temp;
+            }
+
+        }
+
+        public bool isControl()
+        {
+            if (_warn != null)
+            {
+                return false;
+            }
+            else
+            {
+                if (buff[0] == '0' && buff[1] == '0')
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool isData()
+        {
+            if (buff[0] == '0' || buff[1] == '0')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
 }
