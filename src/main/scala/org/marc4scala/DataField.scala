47b9cb11e0364f701d3269a95f6d1a6feccf6c3c
jason.zou@gmail.com <jason.zou@gmail.com>
2016-03-03 09:23:49 -0500

    DataField.scala initial version

--- src/main/scala/org/marc4scala/DataField.scala
+++ src/main/scala/org/marc4scala/DataField.scala
@@ -23,6 +23,21 @@ package org.marc4scala
 /**
   * Created by jason on 2016-02-20.
   */
-trait DataField extends VariableField{
+class DataField (tag:String, val indicator1:Char, val indicator2:Char, 
+                val subfields:List[SubField]) {
+  private var _subfields:List[SubField] = subfields
+  val _tag = new Tag(tag)
+  if (_tag.isControlTag) throw new IllegalStateException("Control Tag in a data field")
 
+  def this(tag:String, indicator1:Char, indicator2:Char) =
+    this(tag, indicator1, indicator2, List())
+
+  def addSubField(subfield:SubField){
+    _subfields = _subfields ::: List(subfield)
+  }
+
+  override def toString:String = {
+    var ret = new String(_tag.toString + indicator1 + indicator2 )
+    return ret
+  }
 }
